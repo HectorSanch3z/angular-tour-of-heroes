1bfa361a00743d6e6ad84b9e6ff3f0a1f890590f
Hector Sanchez <hsanquiroga16@gmail.com>
2024-04-03 10:41:05 -0600

    Solo falta el servidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,22 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, MessagesComponent, HeroesComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HeroDetailComponent],
+  declarations: [
+    AppComponent,
+    MessagesComponent,
+    HeroesComponent,
+    DashboardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HeroDetailComponent,
+    HttpClientModule,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
